0adc9a46c1b9c76683a3d63b43ce84623b512917
Axel Padilla <adpadillar25@gmail.com>
2023-05-13 16:11:05 -0600

    initial videochat version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,134 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useEffect, useRef, useState } from "react";
+
+const FPS = 24;
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState("No data yet");
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  const [lastFrame, setLastFrame] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [room, setRoom] = useState("1234");
+  const [role, setRole] = useState<"send" | "receive">("send");
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-  const handleChange = async () => {
-    const res = await fetch("/api", {
-      body: JSON.stringify({ current: count + 10 }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-    });
-    const text = await res.text();
+  // returns a frame encoded in base64
 
-    setCount((count) => count + 10);
-    setData(text);
+  const getFrame = () => {
+    if (!videoRef.current) return null;
+    const canvas = document.createElement("canvas");
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return null;
+    ctx.drawImage(videoRef.current, 0, 0);
+    const data = canvas.toDataURL("image/png");
+    return data;
   };
+
+  // Initialize video stream
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: { width: 426, height: 240 } })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      });
+  }, []);
+
+  const connectToWs = () => {
+    if (role == "send") {
+      const newWs = new WebSocket("ws://localhost:3000/ws/send");
+      newWs.onopen = () => {
+        setConnected(true);
+        setInterval(() => {
+          newWs.send(JSON.stringify({ id: room, frame: getFrame() }));
+        }, 1000 / FPS);
+      };
+      newWs.onclose = () => setConnected(false);
+
+      setWs(newWs);
+
+      return newWs;
+    } else {
+      const newWs = new WebSocket("ws://localhost:3000/ws/receive");
+      newWs.onopen = () => {
+        setConnected(true);
+        setInterval(() => {
+          newWs.send(JSON.stringify({ id: room, frame: "" }));
+        }, 1000 / FPS);
+      };
+      newWs.onclose = () => setConnected(false);
+
+      newWs.onmessage = (e) => {
+        setLastFrame(e.data);
+      };
+
+      setWs(newWs);
+
+      return newWs;
+    }
+  };
+
+  // Handle disconnection if when we reconnect
+  useEffect(() => {
+    return () => {
+      ws?.close();
+    };
+  }, [ws]);
 
   return (
     <>
+      <h1>Simple Websocket Connection</h1>
+
+      <p>
+        This is a simple websocket connection between a client and a server.
+      </p>
+
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>Status: {connected ? "connected" : "disconnected"}</span>
+        <br />
+        {connected ? (
+          <button onClick={() => ws?.close()}>Disconnect</button>
+        ) : (
+          <button onClick={() => connectToWs()}>Connect</button>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleChange}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <br />
+
+      <div>
+        <span>Chose your role: </span>
+        <br />
+        <select
+          value={role}
+          onChange={(e) => setRole(e.target.value as "send" | "receive")}
+        >
+          <option value="send">Sender</option>
+          <option value="receive">Receiver</option>
+        </select>
       </div>
-      <p className="read-the-docs">{data}</p>
+
+      <p>Room: {room}</p>
+
+      <input value={room} onChange={(e) => setRoom(e.target.value)} />
+
+      <br />
+
+      {role == "receive" ? (
+        <div>
+          <h2>Server Video Feed</h2>
+          <p>This feed is being streamed from room: {room}</p>
+
+          {lastFrame && <img src={lastFrame} alt="last frame" />}
+        </div>
+      ) : (
+        <div>
+          <h2>Local Video Feed</h2>
+          <p>This feed is being streamed to a server</p>
+
+          <video autoPlay ref={videoRef} />
+        </div>
+      )}
     </>
   );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,97 @@
+use async_std::{prelude::*, sync::RwLock};
 use serde::Deserialize;
-use tide::Request;
+use std::{collections::HashMap, sync::Arc};
+use tide::{Body, Request, Response};
+use tide_websockets::{Message, WebSocket};
 
-#[derive(Debug, Deserialize)]
-struct DataReq {
-    current: u16,
+#[derive(Deserialize, Clone, Debug)]
+struct Room {
+    id: String,
+    frame: String,
+}
+
+#[derive(Clone)]
+struct State {
+    rooms: Arc<RwLock<HashMap<String, Room>>>,
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let mut app = tide::new();
+    let state = State {
+        rooms: Default::default(),
+    };
+    let mut app = tide::with_state(state);
 
     match app.at("/").serve_file("client/dist/index.html") {
-        Ok(_) => {
-            print!("Handle OK");
-        }
-        Err(_) => {
-            print!("Handle Err!")
-        }
-    }
-
-    match app.at("/").serve_dir("client/dist/") {
-        Ok(_) => {
-            print!("Handle OK");
-        }
-        Err(_) => {
-            print!("Handle Err!")
-        }
+        Ok(_) => (),
+        Err(e) => println!("Error: {}", e),
     };
 
-    app.at("/api").post(get_data);
+    app.at("/*").get(serve_files);
 
-    app.listen("127.0.0.1:8080").await?;
+    app.at("/ws/send")
+        .get(WebSocket::new(move |req, mut stream| async move {
+            while let Some(Ok(Message::Text(input))) = stream.next().await {
+                let state: &State = req.state();
+                let mut rooms = state.rooms.write().await;
+
+                let frame = match serde_json::from_str::<Room>(input.as_str()) {
+                    Ok(client_room) => {
+                        let room_id = client_room.id.clone();
+                        let room_frame = client_room.frame.clone();
+                        rooms.insert(room_id, client_room);
+
+                        room_frame
+                    }
+                    Err(_e) => String::from(""),
+                };
+
+                stream.send_string(frame).await?;
+            }
+
+            Ok(())
+        }));
+
+    app.at("/ws/receive")
+        .get(WebSocket::new(move |req, mut stream| async move {
+            while let Some(Ok(Message::Text(input))) = stream.next().await {
+                let state: &State = req.state();
+                let rooms = state.rooms.write().await;
+
+                let frame = match serde_json::from_str::<Room>(input.as_str()) {
+                    Ok(client_room) => {
+                        let room_id = client_room.id.clone();
+
+                        match rooms.get(&room_id) {
+                            Some(room) => room.frame.clone(),
+                            None => String::from(""),
+                        }
+                    }
+                    Err(_e) => String::from(""),
+                };
+
+                stream.send_string(frame).await?;
+            }
+
+            Ok(())
+        }));
+
+    app.listen("0.0.0.0:3000").await?;
     Ok(())
 }
 
-async fn get_data(mut req: Request<()>) -> tide::Result {
-    let DataReq { current } = req.body_json().await?;
-    Ok(format!("Hello, from server! Next click is {}", current + 10).into())
+async fn serve_files(req: Request<State>) -> tide::Result {
+    let path = req.url().path();
+
+    let server_path = format!("client/dist/{}", path);
+    let exists = std::path::Path::new(server_path.as_str()).exists();
+
+    let mut res = Response::new(200);
+
+    match exists {
+        true => res.set_body(Body::from_file(server_path).await?),
+        false => res.set_body(Body::from_file("client/dist/index.html").await?),
+    }
+
+    Ok(res)
 }
